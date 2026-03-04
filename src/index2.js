2cfeb7ec64c7459066d673e95a477bc17de389fd
emir budak <emir.budak@epfedu.fr>
2026-03-04 11:59:21 +0100

    4 niveau

--- src/index2.js
+++ src/index2.js
@@ -17,7 +17,7 @@ var config = {
       gravity: {
         y: 300 // gravité verticale : acceleration ddes corps en pixels par seconde
       },
-      debug: false // permet de voir les hitbox et les vecteurs d'acceleration quand mis à true
+      debug: true // permet de voir les hitbox et les vecteurs d'acceleration quand mis à true
     }
   },
   scene: {
@@ -41,14 +41,18 @@ new Phaser.Game(config);
  * On y trouve surtout le chargement des assets (images, son ..)
  */
 function preload() {
-this.load.image("img_ciel", "src/assets/sky.png"); 
-this.load.image("img_plateforme","src/assets/platform.png")
-this.load.spritesheet("img_perso", "src/assets/dude.png", {
+  this.load.image("img_ciel", "src/assets/sky.png");
+  this.load.image("img_plateforme", "src/assets/platform.png")
+  this.load.spritesheet("img_perso", "src/assets/dude.png", {
     frameWidth: 32,
     frameHeight: 48
-  }); 
-this.load.image("img_etoile", "src/assets/star.png"); 
-this.load.image("img_bomb", "src/assets/bomb.png");
+  });
+  this.load.image("img_etoile", "src/assets/star.png");
+  this.load.image("img_bomb", "src/assets/bomb.png");
+  // chargement tuiles de jeu
+  this.load.image("Phaser_tuilesdejeu", "src/assets/tuilesJeu.png");
+  // chargement de la carte
+  this.load.tilemapTiledJSON("carte", "src/assets/map.tmj");
 }
 
 /***********************************************************************/
@@ -62,65 +66,99 @@ this.load.image("img_bomb", "src/assets/bomb.png");
  * ainsi que toutes les instructions permettant de planifier des evenements
  */
 function create() {
-this.add.image(400, 300, "img_ciel"); 
 
-groupe_plateformes=this.physics.add.staticGroup();
-groupe_plateformes.create(200,584, "img_plateforme");
-groupe_plateformes.create(600, 584, "img_plateforme"); 
-groupe_plateformes.create(50, 300, "img_plateforme");
-groupe_plateformes.create(600, 450, "img_plateforme");
-groupe_plateformes.create(750, 270, "img_plateforme");
+  // chargement de la carte
+  const carteDuNiveau = this.add.tilemap("carte");
+  // chargement du jeu de tuiles
+  const tileset = carteDuNiveau.addTilesetImage(
+    "tuiles_de_jeu",
+    "Phaser_tuilesdejeu"
+  );
+  // chargement du calque calque_background
+  Calque_de_background = carteDuNiveau.createLayer(
+    "Calque de background",
+    tileset
+  );
+  // chargement du calque calque_background_2
+  calque_de_background_2 = carteDuNiveau.createLayer(
+    "    de background 2",
+    tileset
+  );
+  // chargement du calque calque_plateformes
+  calque_de_platetorm = carteDuNiveau.createLayer(
+    "calque de platetorm",
+    tileset
+  );
+  // définition des tuiles de plateformes qui sont solides
+  // utilisation de la propriété estSolide
+  calque_de_platetorm.setCollisionByProperty({ estSolide: true })
 
-player = this.physics.add.sprite(100, 450, 'img_perso'); 
-player.setCollideWorldBounds(true); 
-this.physics.add.collider(player, groupe_plateformes);
-player.setBounce(0.2);  
-clavier = this.input.keyboard.createCursorKeys(); 
-this.anims.create({
-  key: "anim_tourne_gauche", // key est le nom de l'animation : doit etre unique poru la scene.
-  frames: this.anims.generateFrameNumbers("img_perso", { start: 0, end: 3 }), // on prend toutes les frames de img perso numerotées de 0 à 3
-  frameRate: 10, // vitesse de défilement des frames
-  repeat: -1 // nombre de répétitions de l'animation. -1 = infini
+  // redimentionnement du monde avec les dimensions calculées via tiled
+  this.physics.world.setBounds(0, 0, 3200, 640);
+  // ajout du champs de la caméra de taille identique à celle du monde
+  this.cameras.main.setBounds(0, 0, 3200, 640);
+  // ancrage de la caméra sur le joueur
+
+  groupe_plateformes = this.physics.add.staticGroup();
+  groupe_plateformes.create(200, 584, "img_plateforme");
+  groupe_plateformes.create(600, 584, "img_plateforme");
+  groupe_plateformes.create(50, 300, "img_plateforme");
+  groupe_plateformes.create(600, 450, "img_plateforme");
+  groupe_plateformes.create(750, 270, "img_plateforme");
+
+  player = this.physics.add.sprite(100, 450, 'img_perso');
+  player.setCollideWorldBounds(true);
+  this.physics.add.collider(player, groupe_plateformes);
+  player.setBounce(0.2);
+  clavier = this.input.keyboard.createCursorKeys();
+  this.anims.create({
+    key: "anim_tourne_gauche", // key est le nom de l'animation : doit etre unique poru la scene.
+    frames: this.anims.generateFrameNumbers("img_perso", { start: 0, end: 3 }), // on prend toutes les frames de img perso numerotées de 0 à 3
+    frameRate: 10, // vitesse de défilement des frames
+    repeat: -1 // nombre de répétitions de l'animation. -1 = infini
   });
-this.anims.create({
-key: "anim_tourne_droite", // key est le nom de l'animation : doit etre unique poru la scene.
-  frames: this.anims.generateFrameNumbers("img_perso", { start: 5, end: 8 }), // on prend toutes les frames de img perso numerotées de 0 à 3
-  frameRate: 10, // vitesse de défilement des frames
-  repeat: -1 // nombre de répétitions de l'animation. -1 = infini
-  });  
-this.anims.create({
-  key: "anim_face",
-  frames: [{ key: "img_perso", frame: 4 }],
-  frameRate: 20
-  }); 
+  this.anims.create({
+    key: "anim_tourne_droite", // key est le nom de l'animation : doit etre unique poru la scene.
+    frames: this.anims.generateFrameNumbers("img_perso", { start: 5, end: 8 }), // on prend toutes les frames de img perso numerotées de 0 à 3
+    frameRate: 10, // vitesse de défilement des frames
+    repeat: -1 // nombre de répétitions de l'animation. -1 = infini
+  });
+  this.anims.create({
+    key: "anim_face",
+    frames: [{ key: "img_perso", frame: 4 }],
+    frameRate: 20
+  });
 
 
-groupe_etoiles = this.physics.add.group(); 
-for (var i = 0; i < 10; i++) {
+  groupe_etoiles = this.physics.add.group();
+  for (var i = 0; i < 10; i++) {
     var coordX = 70 + 70 * i;
     groupe_etoiles.create(coordX, 10, "img_etoile");
-  } 
-this.physics.add.collider(groupe_etoiles, groupe_plateformes); 
-groupe_etoiles.children.iterate(function iterateur(etoile_i) {
+  }
+  this.physics.add.collider(groupe_etoiles, groupe_plateformes);
+  groupe_etoiles.children.iterate(function iterateur(etoile_i) {
     // On tire un coefficient aléatoire de rerebond : valeur entre 0.4 et 0.8
     var coef_rebond = Phaser.Math.FloatBetween(0.4, 0.8);
     etoile_i.setBounceY(coef_rebond); // on attribut le coefficient de rebond à l'étoile etoile_i
-  }); 
-this.physics.add.overlap(player, groupe_etoiles, ramasserEtoile, null, this);
- zone_texte_score = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' }); 
+  });
+  this.physics.add.overlap(player, groupe_etoiles, ramasserEtoile, null, this);
+  zone_texte_score = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
 
 
-groupe_bombes = this.physics.add.group();
-this.physics.add.collider(groupe_bombes, groupe_plateformes);  
-
-
-
- this.physics.add.collider(player, groupe_bombes, chocAvecBombe, null, this); 
+  groupe_bombes = this.physics.add.group();
+  this.physics.add.collider(groupe_bombes, groupe_plateformes);
 
 
 
+  this.physics.add.collider(player, groupe_bombes, chocAvecBombe, null, this);
 
 
+
+  this.cameras.main.startFollow(player);
+
+
+  // ajout d'une collision entre le joueur et le calque plateformes
+  this.physics.add.collider(player, calque_de_platetorm);
 
 
 
@@ -136,23 +174,23 @@ this.physics.add.collider(groupe_bombes, groupe_plateformes);
 /***********************************************************************/
 
 function update() {
-if (clavier.right.isDown) {
+  if (clavier.right.isDown) {
     player.setVelocityX(160);
     player.anims.play('anim_tourne_droite', true);
-  } 
-  else if ( clavier.left.isDown) {
-    player.setVelocityX(-160); 
-    player.anims.play('anim_tourne_gauche', true); 
+  }
+  else if (clavier.left.isDown) {
+    player.setVelocityX(-160);
+    player.anims.play('anim_tourne_gauche', true);
   } else {
     player.setVelocityX(0);
-    player.anims.play('anim_face'); 
-  } 
-if (clavier.space.isDown  && player.body.touching.down){
-  player.setVelocityY(-330);
-}
-if (gameOver) {
+    player.anims.play('anim_face');
+  }
+  if (clavier.up.isDown && player.body.blocked.down) {
+    player.setVelocityY(-330);
+  }
+  if (gameOver) {
     return;
-  } 
+  }
 
 
 }
@@ -163,7 +201,7 @@ if (gameOver) {
 /************************ */
 var groupe_plateformes;
 
-
+ 
 
 /*********************************** */
 /** GROUPE PERSO
@@ -183,14 +221,14 @@ function ramasserEtoile(un_player, une_etoile) {
   // on désactive le "corps physique" de l'étoile mais aussi sa texture
   // l'étoile existe alors sans exister : elle est invisible et ne peut plus intéragir
   une_etoile.disableBody(true, true);
-if (groupe_etoiles.countActive(true) === 0) {
+  if (groupe_etoiles.countActive(true) === 0) {
     // si ce nombre est égal à 0 : on va réactiver toutes les étoiles désactivées
     // pour chaque étoile etoile_i du groupe, on réacttive etoile_i avec la méthode enableBody
     // ceci s'ecrit bizarrement : avec un itérateur sur les enfants (children) du groupe (equivalent du for)
     groupe_etoiles.children.iterate(function iterateur(etoile_i) {
       etoile_i.enableBody(true, etoile_i.x, 0, true, true);
     });
-     var x;
+    var x;
     if (player.x < 400) {
       x = Phaser.Math.Between(400, 800);
     } else {
@@ -203,19 +241,19 @@ if (groupe_etoiles.countActive(true) === 0) {
     une_bombe.setVelocity(Phaser.Math.Between(-200, 200), 20);
     une_bombe.allowGravity = false;
   }
-score += 1;
-zone_texte_score.setText("Score: " + score); 
-} 
+  score += 1;
+  zone_texte_score.setText("Score: " + score);
+}
 /*********************************** */
 /** GROUPE score
 /************************ */
 var score = 0;
-var zone_texte_score; 
+var zone_texte_score;
 /*********************************** */
 /** GROUPE BOMBES
 /************************ */
 
-var gameOver = false; 
+var gameOver = false;
 var groupe_bombes;
 function chocAvecBombe(un_player, une_bombe) {
   this.physics.pause();
